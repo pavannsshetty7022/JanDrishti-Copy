743e7d8bdf70235ffa1e94861e14a4c8645bb55f
pavannsshetty7022 <shettypavan524@gmail.com>
2026-01-26 10:31:21 +0530

    e

--- backend/generateAdminHash.js
+++ backend/generateAdminHash.js
@@ -1,25 +1,14 @@
-const bcrypt = require('bcryptjs');
-
-
+import bcrypt from 'bcryptjs';
 
 const newAdminPassword = 'Pk@7022302564'; 
-
 const saltRounds = 10;
 
-
-
 bcrypt.hash(newAdminPassword, saltRounds, (err, hash) => {
-
     if (err) {
-
         console.error('Error hashing password:', err);
-
         return;
-
     }
 
     console.log('New Admin Username: pavannsshetty'); 
-
     console.log('Hashed Password:', hash);
-
 });
